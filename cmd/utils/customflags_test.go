30865ae7606f0e6b18973315fae4e69e463626a5
aurorachainio <59164220+aurorachainio@users.noreply.github.com>
2023-06-02 08:39:12 +0800

    fix parameter count of miner_start

--- cmd/utils/customflags_test.go
+++ cmd/utils/customflags_test.go
@@ -31,6 +31,7 @@ func TestPathExpansion(t *testing.T) {
 		"$DDDXXX/a/b":        "/tmp/a/b",
 		"/a/b/":              "/a/b",
 	}
+	
 	os.Setenv("DDDXXX", "/tmp")
 	for test, expected := range tests {
 		got := expandPath(test)
